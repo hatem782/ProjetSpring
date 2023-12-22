0bc8f3abcdf349526b6f2c6d407ab8152a77a142
hatem782 <hatembenechikh100@gmail.com>
2023-12-22 15:35:34 +0100

    fix realtion auteur livre

--- bibleo-app/src/main/java/com/isamm/bibleoapp/Entity/Auteur.java
+++ bibleo-app/src/main/java/com/isamm/bibleoapp/Entity/Auteur.java
@@ -1,10 +1,9 @@
 package com.isamm.bibleoapp.Entity;
 
-
-
 import java.util.Date;
+import java.util.List;
 
-
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -12,168 +11,110 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="AUTEUR")
+@Table(name = "AUTEUR")
 public class Auteur {
-    
 
- @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
 
+  private String nom;
+  private String prenom;
+  private Date dateNaiss;
+  private String nationalite;
 
-    private String nom;
-    private String prenom;
-    private Date dateNaiss;
-    private String nationalite;
+  @Enumerated(EnumType.STRING)
+  private AuteurStatut auteurStatut;
 
+  @OneToMany(mappedBy = "auteur", cascade = CascadeType.ALL)
+  private List<Livre> livres;
 
-    @Enumerated(EnumType.STRING)
-    private AuteurStatut auteurStatut;
+  // ################### CONSTUCTORS ###################
 
+  public Auteur() {
 
+  }
 
-    @ManyToOne
-    @JoinColumn(name = "livre_id")
-    private Livre livre;
+  // ################### CONSTUCTORS without livre ###################
 
+  public Auteur(String nom, String prenom, Date dateNaiss, String nationalite,
+      AuteurStatut auteurStatut) {
 
-  
+    this.nom = nom;
+    this.prenom = prenom;
+    this.dateNaiss = dateNaiss;
+    this.nationalite = nationalite;
+    this.auteurStatut = auteurStatut;
 
-     // ################### CONSTUCTORS ###################
+  }
 
-     public Auteur() {
-   
-    }
-    
-    
-    
-    
-    public Auteur(String nom, String prenom, Date dateNaiss, String nationalite, 
-    AuteurStatut auteurStatut,Livre livre) {
-   
-      this.nom = nom;
-      this.prenom = prenom;
-      this.dateNaiss = dateNaiss;
-      this.nationalite = nationalite;
-      this.auteurStatut = auteurStatut;
-      this.livre=livre;
-    }
-    
-     // ################### CONSTUCTORS without livre ###################
+  // ################### GETTERS & SETTERS ###################
 
-     public Auteur(String nom, String prenom, Date dateNaiss, String nationalite, 
-    AuteurStatut auteurStatut) {
-   
-      this.nom = nom;
-      this.prenom = prenom;
-      this.dateNaiss = dateNaiss;
-      this.nationalite = nationalite;
-      this.auteurStatut = auteurStatut;
+  public Long getId() {
+    return id;
+  }
 
-    }
-    
+  public void setId(Long id) {
+    this.id = id;
+  }
 
- // ################### GETTERS & SETTERS ###################
+  public String getNom() {
+    return nom;
+  }
 
-public Long getId() {
-  return id;
-}
+  public void setNom(String nom) {
+    this.nom = nom;
+  }
 
-public void setId(Long id) {
-  this.id = id;
-}
+  public String getPrenom() {
+    return prenom;
+  }
 
- 
-public String getNom() {
-	return nom;
-}
+  public void setPrenom(String prenom) {
+    this.prenom = prenom;
+  }
 
+  public Date getDateNaiss() {
+    return dateNaiss;
+  }
 
+  public void setDateNaiss(Date dateNaiss) {
+    this.dateNaiss = dateNaiss;
+  }
 
+  public String getNationalite() {
+    return nationalite;
+  }
 
-public void setNom(String nom) {
-	this.nom = nom;
-}
+  public void setNationalite(String nationalite) {
+    this.nationalite = nationalite;
+  }
 
+  public AuteurStatut getAuteurStatut() {
+    return auteurStatut;
+  }
 
+  public void setAutheurStatut(AuteurStatut auteurStatut) {
+    this.auteurStatut = auteurStatut;
+  }
 
+  public List<Livre> getLivres() {
+    return livres;
+  }
 
-public String getPrenom() {
-	return prenom;
-}
-
-
-
-
-public void setPrenom(String prenom) {
-	this.prenom = prenom;
-}
-
-
-
-
-public Date getDateNaiss() {
-	return dateNaiss;
-}
-
-
-
-
-public void setDateNaiss(Date dateNaiss) {
-	this.dateNaiss = dateNaiss;
-}
-
-
-
-
-public String getNationalite() {
-	return nationalite;
-}
-
-
-
-
-public void setNationalite(String nationalite) {
-	this.nationalite = nationalite;
-}
-
-
-
-
-public AuteurStatut getAuteurStatut() {
-	return auteurStatut;
-}
-
-
-
-
-public void setAutheurStatut(AuteurStatut auteurStatut) {
-	this.auteurStatut = auteurStatut;
-}
-
-
-  public Livre  getLivre() {
-	return livre;
-}
-
-public void setLivre(Livre livre) {
-	this.livre = livre;
-}
-
-
-
+  public void setLivres(List<Livre> livres) {
+    this.livres = livres;
+  }
 
   // ################### TO STRING ###################
   @Override
-public String toString() {
-	return "Auteur [nom=" + nom + ", prenom=" + prenom + ", nationalite=" + nationalite  +", livre="+livre+"]";
-}
-
-
-
+  public String toString() {
+    return "Auteur [nom=" + nom + ", prenom=" + prenom + ", nationalite=" + nationalite + ", livres="
+        + livres.toString() + "]";
+  }
 
 }
--- bibleo-app/src/main/java/com/isamm/bibleoapp/Entity/Livre.java
+++ bibleo-app/src/main/java/com/isamm/bibleoapp/Entity/Livre.java
@@ -9,13 +9,13 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="LIVRE")
+@Table(name = "LIVRE")
 public class Livre {
-    
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -29,12 +29,10 @@ public class Livre {
     private float amendeParJour;
     private String imageUri;
     private String genre;
-@Enumerated(EnumType.STRING)
+    @Enumerated(EnumType.STRING)
     private Langue langue;
 
-
-
-     @OneToMany(mappedBy = "livre")
+    @OneToMany(mappedBy = "livre")
     private List<Commentaire> commentaires;
 
     @OneToMany(mappedBy = "livre")
@@ -43,186 +41,172 @@ public class Livre {
     @OneToMany(mappedBy = "livre")
     private List<Emprunt> emprunts;
 
-    @OneToMany(mappedBy = "livre")
-    private List<Auteur> auteurs;
-// ################### CONSTUCTORS ###################
-public Livre() {
-}
+    @ManyToOne
+    private Auteur auteur;
 
-public Livre(String titre
-    , Date anneePub
-    , String isbn
-    , String description
-    , int quantite
-    , float amendeParJour
-    , String imageUri
-    , String genre,Langue langue,
-    List<Commentaire> commentaires,
-    List<Review> reviews
-    ,List<Emprunt> emprunts 
-     ,List<Auteur> auteurs) {
-  this.titre=titre;
-      this.anneePub=anneePub;
-     this.isbn=isbn;
+    // ################### CONSTUCTORS ###################
+    public Livre() {
+    }
 
-     this.description=description;
-      this.quantite=quantite;
-      this.amendeParJour=amendeParJour;
-    this.imageUri=imageUri;
-     this.genre=genre;
-     this.langue = langue;
-     this.commentaires=commentaires;
-      this.reviews=reviews;
-       this.emprunts=emprunts;
-        this.auteurs=auteurs;
+    public Livre(String titre, Date anneePub, String isbn, String description, int quantite, float amendeParJour,
+            String imageUri, String genre, Langue langue,
+            List<Commentaire> commentaires,
+            List<Review> reviews, List<Emprunt> emprunts, Auteur auteur) {
+        this.titre = titre;
+        this.anneePub = anneePub;
+        this.isbn = isbn;
 
-}
+        this.description = description;
+        this.quantite = quantite;
+        this.amendeParJour = amendeParJour;
+        this.imageUri = imageUri;
+        this.genre = genre;
+        this.langue = langue;
+        this.commentaires = commentaires;
+        this.reviews = reviews;
+        this.emprunts = emprunts;
+        this.auteur = auteur;
+    }
 
+    // ################### CONSTUCTORS without reviews,
+    // commentaires,emprunts,auteur###################
 
-// ################### CONSTUCTORS without reviews, commentaires,emprunts,auteur###################
+    public Livre(String titre, Date anneePub, String isbn, String description, int quantite, float amendeParJour,
+            String imageUri, String genre, Langue langue) {
+        this.titre = titre;
+        this.anneePub = anneePub;
+        this.isbn = isbn;
 
-public Livre(String titre
-    , Date anneePub
-    , String isbn
-    , String description
-    , int quantite
-    , float amendeParJour
-    , String imageUri
-    , String genre,Langue langue) {
-  this.titre=titre;
-      this.anneePub=anneePub;
-     this.isbn=isbn;
+        this.description = description;
+        this.quantite = quantite;
+        this.amendeParJour = amendeParJour;
+        this.imageUri = imageUri;
+        this.genre = genre;
+        this.langue = langue;
 
-     this.description=description;
-      this.quantite=quantite;
-      this.amendeParJour=amendeParJour;
-    this.imageUri=imageUri;
-     this.genre=genre;
-     this.langue = langue;
+    }
 
+    // ################### GETTERS ###################
 
- 
+    public Long getId() {
+        return id;
+    }
 
-}
+    public Langue getLangue() {
+        return langue;
+    }
 
-// ################### GETTERS ###################
+    public String getDescription() {
+        return description;
+    }
 
-public Long getId() {
-    return id;
-}
+    public Date getAnneePub() {
+        return anneePub;
+    }
 
-public Langue getLangue() {
-    return langue;
-}
+    public String getIsbn() {
+        return isbn;
+    }
 
-public String getDescription() {
-    return description;
-}
+    public int getQuantite() {
+        return quantite;
+    }
 
-public Date getAnneePub() {
-    return anneePub;
-}
+    public float getAmendeParJour() {
+        return amendeParJour;
+    }
 
+    public String getImageUri() {
+        return imageUri;
+    }
 
-public String getIsbn() {
-    return isbn;
-}
-public int  getQuantite() {
-    return quantite;
-}
+    public String getGenre() {
+        return genre;
+    }
 
-public float  getAmendeParJour() {
-    return amendeParJour;
-}
+    public List<Commentaire> getCommentaires() {
+        return commentaires;
+    }
 
-public String getImageUri() {
-    return imageUri;
-}
+    public List<Review> getReviews() {
+        return reviews;
+    }
 
-public String getGenre() {
-    return genre;
-}
+    public List<Emprunt> getEmprunts() {
+        return emprunts;
+    }
 
-public List<Commentaire> getCommentaires() {
-	return commentaires;
-}
-public List<Review> getReviews() {
-	return reviews;
-}
-public List<Emprunt> getEmprunts() {
-	return emprunts;
-}
-public List<Auteur>  getAuteurs() {
-	return auteurs;
-}
+    public Auteur getAuteur() {
+        return auteur;
+    }
 
-// ################### SETTERS ###################
+    // ################### SETTERS ###################
 
-public void setId(Long id) {
-    this.id = id;
-}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-public void setLangue(Langue langue) {
-    this.langue = langue;
-}
+    public void setLangue(Langue langue) {
+        this.langue = langue;
+    }
 
-public void setTitre(String titre) {
-    this.titre = titre;
-}
-public void setAnneePub(Date anneePub) {
-    this.anneePub = anneePub;
-}
-public void setIsbn(String isbn) {
-    this.isbn = isbn;
-}
-public void setDescription(String description) {
-    this.description = description;
-}
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
 
+    public void setAnneePub(Date anneePub) {
+        this.anneePub = anneePub;
+    }
 
-public void setCommentaires(List<Commentaire> commentaires) {
-	this.commentaires = commentaires;
-}
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-public void setReviews(List<Review> reviews) {
-	this.reviews = reviews;
-}
+    public void setCommentaires(List<Commentaire> commentaires) {
+        this.commentaires = commentaires;
+    }
 
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
 
-public void setEmprunts(List<Emprunt> emprunts) {
-	this.emprunts = emprunts;
-}
+    public void setEmprunts(List<Emprunt> emprunts) {
+        this.emprunts = emprunts;
+    }
 
+    public void setAuteur(Auteur auteur) {
+        this.auteur = auteur;
+    }
 
+    public void setQuantite(int quantite) {
+        this.quantite = quantite;
+    }
 
-public void setAuteur(List<Auteur> auteurs ) {
-	this.auteurs = auteurs;
-}
+    public void setAmendeParJour(float amendeParJour) {
+        this.amendeParJour = amendeParJour;
+    }
 
-public void setQuantite(int quantite) {
-    this.quantite = quantite;
-}
-public void setAmendeParJour(float amendeParJour) {
-    this.amendeParJour = amendeParJour;
-}
-public void setImageUri(String imageUri) {
-    this.imageUri = imageUri;
-}
-public void setGenre (String genre) {
-    this.genre = genre;
-}
+    public void setImageUri(String imageUri) {
+        this.imageUri = imageUri;
+    }
 
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 
-// ################### TO STRING ###################
+    // ################### TO STRING ###################
 
-@Override
-public String toString() {
-	return "Livre [id=" + id + ", titre=" + titre + ", anneePub=" + anneePub + ", isbn=" + isbn + ", description="
-			+ description + ", quantite=" + quantite + ", amendeParJour=" + amendeParJour + ", imageUri=" + imageUri
-			+ ", genre=" + genre + "]"+ ", commentaires=" + commentaires +
-            ", reviews=" + reviews +
-            ", emprunts=" + emprunts +
-            ", auteur=" + auteurs ;
-}
+    @Override
+    public String toString() {
+        return "Livre [id=" + id + ", titre=" + titre + ", anneePub=" + anneePub + ", isbn=" + isbn + ", description="
+                + description + ", quantite=" + quantite + ", amendeParJour=" + amendeParJour + ", imageUri=" + imageUri
+                + ", genre=" + genre + "]" + ", commentaires=" + commentaires.toString() +
+                ", reviews=" + reviews.toString() +
+                ", emprunts=" + emprunts.toString() +
+                ", auteur=" + auteur.toString();
+    }
 }
