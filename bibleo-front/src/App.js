c53f988e0d6ec156b79f06bc88283c6c3bb62013
hatem782 <hatembenechikh100@gmail.com>
2023-12-25 15:06:27 +0100

    preparing dashboard

---
+++ bibleo-front/src/App.js
@@ -0,0 +1,22 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Login from "./pages/Login/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/dashboard/*" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/*" element={<Navigate to="/login" />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
