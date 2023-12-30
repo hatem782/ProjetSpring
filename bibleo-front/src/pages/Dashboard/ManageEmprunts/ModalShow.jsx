46ad1a3808583820b0d900461d9096add4fe1a4c
hatem782 <hatembenechikh100@gmail.com>
2023-12-30 13:40:55 +0100

    show emprunt

--- bibleo-front/src/pages/Dashboard/ManageEmprunts/ModalShow.jsx
+++ bibleo-front/src/pages/Dashboard/ManageEmprunts/ModalShow.jsx
@@ -6,15 +6,41 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogActions from "@mui/material/DialogActions";
 import Button from "@mui/material/Button";
 import { makeDate } from "../../../functions/Dates.functions";
+import Chip from "../../../components/Chip/Chip";
+
+const MakeChipStatus = ({ row }) => {
+  // pending
+  if (row.statut === "EnAttente")
+    return <Chip label="En Attente" color="warning" />;
+
+  // refused
+  if (row.statut === "Refusé") return <Chip label="Refused" color="error" />;
+
+  // accepted
+  if (row.statut === "Emprunté")
+    return <Chip label="Emprunted" color="success" />;
+
+  // done
+  if (row.statut === "Retourné") return <Chip label="Done" color="info" />;
+
+  // returned retard
+  if (row.statut === "Retard" && row.amende === 0)
+    return <Chip label="Retard" color="secondary" />;
+
+  if (row.statut === "Retard" && row.amende !== 0)
+    return <Chip label="Retard & Amande" color="error" />;
+
+  return <Chip label="Retard " color="error" />;
+};
 
 function ModalDelete({ popup, handleClose }) {
-  const { open: book } = popup;
+  const { open: emprunt } = popup;
 
   return (
     <Dialog
-      open={book !== null}
+      open={emprunt !== null}
       handleClose={handleClose}
-      title={"Show Detail Book"}
+      title={"Show Detail Emprunt"}
       width="md"
     >
       <DialogContent dividers data-test="modal">
@@ -29,38 +55,57 @@ function ModalDelete({ popup, handleClose }) {
               justifyContent: "center",
             }}
           >
-            <img src={book.imageUri} alt="" style={{ width: "100%" }} />
+            <img
+              src={emprunt.livre.imageUri}
+              alt=""
+              style={{ width: "100%" }}
+            />
           </Grid>
 
           <Grid item xs={12} sm={6}>
+            <h3>About Emprunt</h3>
+            <Item title="Start-End Date">
+              {makeDate(emprunt.dateDebut)} - {makeDate(emprunt.dateDebut)}
+            </Item>
+            <Divider />
+            <Item title="Return Date">
+              {emprunt.dateDebut ? makeDate(emprunt.dateDebut) : "Not Yet"}
+            </Item>
+            <Divider />
+            <Item title="Number Of Copies Emprunted">
+              {emprunt.nbCopie} - copie {emprunt.nbCopie > 1 ? "s" : ""}
+            </Item>
+
+            <Divider />
+            <Item title="Amande Value : ">{emprunt.amende}.00DT</Item>
+
+            <Divider />
+            <Item title="Emprunt Status">
+              {" "}
+              <MakeChipStatus row={emprunt} />
+            </Item>
+            <Divider />
+            <h3>About Adherant</h3>
+            <Item title="Adherant FullName"> {emprunt.adherent.fullname} </Item>
+            <Divider />
+            <Item title="Adherant Email"> {emprunt.adherent.email} </Item>
+            <Divider />
+            <Item title="Adherant Phone Number">
+              +216 {emprunt.adherent.phone}
+            </Item>
+            <Divider />
+
             <h3>About Book</h3>
-            <Item title="Book Title"> {book.titre} </Item>
+            <Item title="Book Title"> {emprunt.livre.titre} </Item>
             <Divider />
             <Item title="ID / ISBN ">
-              {book.id} / {book.isbn}
+              {emprunt.livre.id} / {emprunt.livre.isbn}
             </Item>
             <Divider />
             <Item title="Quantity / Amandes Par Jour">
-              {book.quantite} / {book.amendeParJour}DT
+              {emprunt.livre.quantite} / {emprunt.livre.amendeParJour}DT
             </Item>
             <Divider />
-            <Item title="Genre / Langue">
-              {book.genre} / {book.langue}
-            </Item>
-            <Divider />
-            <Item title="Year of publish">{makeDate(book.anneePub)}</Item>
-            <Divider />
-            <Item title="Description">{book.description}</Item>
-
-            <h3>About Auther</h3>
-
-            <Item title="ID">{book.auteur.id}</Item>
-            <Item title="Full Name">
-              {book.auteur.nom} {book.auteur.prenom}
-            </Item>
-            <Item title="Birth Date">{makeDate(book.auteur.dateNaiss)}</Item>
-            <Item title="Nationality">{book.auteur.nationalite}</Item>
-            <Item title="Status">{book.auteur.auteurStatut}</Item>
           </Grid>
         </Grid>
       </DialogContent>
