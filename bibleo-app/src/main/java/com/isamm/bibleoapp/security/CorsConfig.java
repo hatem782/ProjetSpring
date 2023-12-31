f38d3bde77336e123d02f4608a9d83cb06122973
hatem782 <hatembenechikh100@gmail.com>
2023-12-31 15:48:18 +0100

    fix cors

--- bibleo-app/src/main/java/com/isamm/bibleoapp/security/CorsConfig.java
+++ bibleo-app/src/main/java/com/isamm/bibleoapp/security/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // Add your ReactJS app's URL
+                        .allowedOrigins("*") // Add your ReactJS app's URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
